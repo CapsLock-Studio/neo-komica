8aabdbe216337fbbb34eb5bbc5b4fb34e991eaf5
Calvin Huang <calvin.peak@capslock.tw>
2020-06-20 18:12:04 +0800

    feat: add media model

---
+++ model/media.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+// MediaType - the Media type deinifition
+type MediaType int
+
+const (
+	// MediaTypeImage - Post is image
+	MediaTypeImage MediaType = 0
+	// MediaTypeVideo - Post is video
+	MediaTypeVideo MediaType = 1
+)
+
+// Media - the model of image/video
+type Media struct {
+	gorm.Model
+
+	Type MediaType
+	// Image or video source URL
+	RawSrc *string `gorm:"type:text"`
+
+	UUID     uuid.UUID `gorm:"type:char(36);unique_index;not null"`
+	FileType string
+	Height   uint
+	Width    uint
+
+	Posts []Post `gorm:"many2many:post_media_map"`
+}
