a197eb349e7e219981bd9e05e577c69247874c1a
Calvin Huang <calvin.peak@capslock.tw>
2020-06-20 18:11:38 +0800

    chore: add topic migration

---
+++ model/migration/20200619015206_add_topic.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+
+	"github.com/CapsLock-Studio/neo-komica/model"
+)
+
+type migration string
+
+// Up - Changes for the migration.
+func (m migration) Up(db *gorm.DB) error {
+	db.AutoMigrate(
+		&model.Topic{},
+	)
+	return nil
+}
+
+// Down - Rollback changes for the migration.
+func (m migration) Down(db *gorm.DB) error {
+	db.DropTableIfExists(&model.Topic{})
+	return nil
+}
+
+var Migration migration
