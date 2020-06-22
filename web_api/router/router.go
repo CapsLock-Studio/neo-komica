593150498fd44b61db06c425035f86e20d0846ea
Calvin Huang <calvin.peak@capslock.tw>
2020-06-22 17:42:54 +0800

    chore: register post to route

--- web_api/router/router.go
+++ web_api/router/router.go
@@ -3,12 +3,13 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 
-	"github.com/CapsLock-Studio/neo-komica/web_api/router/v1/foo"
+	"github.com/CapsLock-Studio/neo-komica/web_api/router/v1/post"
 )
 
 // AddRouter - Add nested api routers to app.
 func AddRouter(engine *gin.Engine) {
 	r := engine.Group("/api/v1")
 
-	r.GET("/foo", foo.Index)
+	r.GET("/post", post.Index)
+	r.GET("/post/:parent/more", post.More)
 }
