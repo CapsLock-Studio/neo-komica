73cb97b65ff02ac7bb747e00c031de5d2821f5bc
Calvin Huang <calvin.peak@capslock.tw>
2020-06-24 14:13:12 +0800

    feat: add postorder and replyorder scopes

--- model/post.go
+++ model/post.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"github.com/jinzhu/gorm"
 	uuid "github.com/satori/go.uuid"
 )
 
@@ -17,4 +18,14 @@ type Post struct {
 
 	Medias  []Media `gorm:"many2many:post_media_map"`
 	Replies []Post  `gorm:"foreignkey:parent_id"`
+}
+
+// PostOrder - scope for post order.
+func PostOrder(db *gorm.DB) *gorm.DB {
+	return db.Order("id DESC")
+}
+
+// ReplyOrder - scope for reply order.
+func ReplyOrder(db *gorm.DB) *gorm.DB {
+	return db.Order("id ASC")
 }
